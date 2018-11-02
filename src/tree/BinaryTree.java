29d6965d1e6bcaa79185f2175fc692d003920b71
Igor <smprog@mail.ru>
2018-11-02 15:11:02 +0300

    +count +contains -вывод дерева

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -1,72 +1,60 @@
 package tree;
 
+import java.util.ArrayList;
+
 class BinaryTree {
 
-    private Node root;
-    int maxVLevel = 0;
-    int maxHLevel = 0;
-    final static String STR_SEQ = "     ";
-
-    Node getRoot() {
-        return root;
-    }
+    private ArrayList<Integer> treeData = new ArrayList<>();
 
     BinaryTree(int[] data, int rootValue) {
 
-        root = new Node(rootValue, 0, 5);
+        Node root = new Node(rootValue);
+        treeData.add(rootValue);
 
         for (int newVal : data) {
             addNode(newVal, root);
         }
     }
 
-    void calcuateOutputArray(Node node, String[][] matrix) {
-        matrix[node.getvLevel()][node.gethLevel()] = matrix[node.getvLevel()][node.gethLevel()].equals(STR_SEQ) ? String.valueOf(node.getValue()) : matrix[node.getvLevel()][node.gethLevel()] + "|" + String.valueOf(node.getValue());
-        if (node.getLeft() != null) {
-            calcuateOutputArray(node.getLeft(), matrix);
-        }
-        if (node.getRight() != null) {
-            calcuateOutputArray(node.getRight(), matrix);
-        }
-    }
-
     private void addNode(int newVal, Node node) {
-        int newVLevel = node.getvLevel() + 1;
-        int newHLevel = node.gethLevel();
-
         if (newVal < node.getValue()) {
             if (node.getLeft() == null) {
-                if (maxVLevel < newVLevel) {
-                    maxVLevel = newVLevel;
-                }
-                node.setLeft(new Node(newVal, newVLevel, newHLevel - 1));
+                treeData.add(newVal);
+                node.setLeft(new Node(newVal));
             } else {
                 addNode(newVal, node.getLeft());
             }
         } else {
             if (node.getRight() == null) {
-                maxHLevel = newHLevel + 1;
-                if (maxVLevel < newVLevel) {
-                    maxVLevel = newVLevel;
-                }
-                node.setRight(new Node(newVal, newVLevel, newHLevel + 1));
+                treeData.add(newVal);
+                node.setRight(new Node(newVal));
             } else {
                 addNode(newVal, node.getRight());
             }
         }
     }
 
+    boolean contain(int val) {
+        return treeData.contains(val);
+    }
+
+    int count(int val) {
+        int found = 0;
+        for (int i : treeData) {
+            if (i == val) {
+                found++;
+            }
+        }
+        return found;
+    }
+
     private class Node {
         private Node left = null;
         private Node right = null;
         private int value;
-        private int vLevel;
-        private int hLevel;
 
-        Node(int val, int vLevel, int hLevel) {
+        Node(int val) {
             this.setValue(val);
-            this.setvLevel(vLevel);
-            this.sethLevel(hLevel);
         }
 
         Node getLeft() {
@@ -91,22 +79,6 @@ class BinaryTree {
 
         void setValue(int value) {
             this.value = value;
-        }
-
-        int getvLevel() {
-            return vLevel;
-        }
-
-        void setvLevel(int vLevel) {
-            this.vLevel = vLevel;
-        }
-
-        int gethLevel() {
-            return hLevel;
-        }
-
-        void sethLevel(int hLevel) {
-            this.hLevel = hLevel;
         }
 
     }
--- src/tree/UseTree.java
+++ src/tree/UseTree.java
@@ -4,23 +4,13 @@ public class UseTree {
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree(new int[]{1, 4, 8, 3, 9, 0, 2, -1, 1, 5, 2, 5, 9}, 5);
 
-        String[][] matrix = new String[tree.maxVLevel + 1][tree.maxHLevel + 1];
-        for (int i = 0; i < tree.maxVLevel + 1; i++) {
-            for (int j = 0; j < tree.maxHLevel + 1; j++) {
-                matrix[i][j] = BinaryTree.STR_SEQ;
-            }
-        }
-        tree.calcuateOutputArray(tree.getRoot(), matrix);
+        int searchNode = 5;
+        if (tree.contain(searchNode)) {
+            System.out.println("Значение " + searchNode + " найдено в количестве "
+                    + tree.count(searchNode) + " шт.");
+        } else {
+            System.out.println("Значение " + searchNode + " не найдено");
 
-        for (int i = 0; i < tree.maxVLevel + 1; i++) {
-            for (int j = 0; j < tree.maxHLevel + 1; j++) {
-                StringBuilder sb = new StringBuilder();
-                for (int k = 1; k <= 5 - matrix[i][j].length(); k++) {
-                    sb.append(" ");
-                }
-                System.out.print(matrix[i][j] + sb.toString());
-            }
-            System.out.println();
         }
     }
 }
